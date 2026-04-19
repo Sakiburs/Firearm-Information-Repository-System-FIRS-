2c48a13f01c1e39eb7e83c310ad4efb25e29752c
Mahfuz Shaikot <mahfuzshaikot472@gmail.com>
2026-04-19 11:59:11 +0600

    Delete src/main directory

--- src/main/java/com/firs/project/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package com.firs.project.controller;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/users")
-@CrossOrigin
-public class UserController {
-}
--- src/main/java/com/firs/project/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firs.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.firs.project.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
